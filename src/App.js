af2ba95db78018ee13c9efd8cc30bcdcfd0f43b9
User <User@DESKTOP-C51DDIB>
2023-05-16 14:25:06 +0500

    shoppingcart

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import { useSelector, useDispatch } from 'react-redux';
-import './App.css';
-
-function App() {
-
-  const counter = useSelector((state) => state.counter);
-
-  const dispatch = useDispatch();
-
-  const increment = () => {
-    dispatch({type: 'INC'});
-
-  }
-  const decrement = () => {
-    dispatch({type: 'DEC'});
-    
-  }
-
-  const addBy = () => {
-    dispatch({type:'ADD', payload: 10});
-  }
-
-  return (
-    <div className="App">
-
-      <h1>Counter App</h1>
-      <h2>{counter}</h2>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={addBy}>Add by 10</button>
-      
-=======
 import React from "react";
 import "./App.css";
 import Auth from "./components/Auth";
@@ -41,7 +8,6 @@ function App() {
     <div className="App">
       <Auth />
       {/* <Layout /> */}
->>>>>>> StartingApp
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -38,4 +15,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
->>>>>>> StartingApp
